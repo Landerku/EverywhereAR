07b32df55b147a11cb6feff9e28d50bd5f57f883
Landerku <1025liujia@gmail.com>
2023-12-18 23:43:04 +0900

    Third
    Delete solution floder

--- Solution/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Unity_project\SceneUnderstanding1\Solution\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "D:\Unity_project\SceneUnderstanding1\Solution\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "D:\Unity_project\SceneUnderstanding1\Solution\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
