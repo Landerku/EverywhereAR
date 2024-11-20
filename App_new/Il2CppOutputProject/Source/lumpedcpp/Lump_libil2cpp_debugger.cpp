e76682349d73a81e4f42d2dab98cf5802a7c27e4
Landerku <1025liujia@gmail.com>
2024-11-20 21:41:14 +0900

    Delete App Folder

--- App_new/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- App_new/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\MonoPosixHelper.cpp"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolMonitorThread.cpp"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolWorkerThread.cpp"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io-poll.cpp"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io.cpp"
-#include "D:\GitHub\EveryWhereAR_repo\App_new\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms.cpp"
